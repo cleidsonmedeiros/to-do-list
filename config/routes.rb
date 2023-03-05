a56dab55c768dc3676fb18838ae6b09388c26da7
cleidsonmedeiros <cleidsonmedeiros12@gmail.com>
2023-03-05 19:47:47 -0300

    fix tasks route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :tasks, only[:new, :create]
+  resources :tasks, only: [:new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
