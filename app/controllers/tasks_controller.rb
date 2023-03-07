b03eea21d0aa11eb62dd83852cac0e91006d1801
cleidsonmedeiros <cleidsonmedeiros12@gmail.com>
2023-03-07 07:36:55 -0300

    implement respond_to_format function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,63 +1,67 @@
 class TasksController < ApplicationController
-    before_action :authenticate_user!
-    before_action :set_task, only: [:show, :edit, :update, :destroy]
-  
-    def index
-      @tasks = current_user.tasks.all
-    end
-  
-    def show
-    end
-  
-    def new
-      @task = current_user.tasks.build
-    end
-  
-    def edit
-    end
-  
-    def create
-      @task = current_user.tasks.build(task_params)
-  
-      respond_to do |format|
-        if @task.save
-          format.html { redirect_to @task, notice: 'Task was successfully created.' }
-          format.json { render :show, status: :created, location: @task }
-        else
-          format.html { render :new }
-          format.json { render json: @task.errors, status: :unprocessable_entity }
-        end
-      end
-    end
-  
-    def update
-      respond_to do |format|
-        if @task.update(task_params)
-          format.html { redirect_to @task, notice: 'Task was successfully updated.' }
-          format.json { render :show, status: :ok, location: @task }
-        else
-          format.html { render :edit }
-          format.json { render json: @task.errors, status: :unprocessable_entity }
-        end
-      end
-    end
-  
-    def destroy
-      @task.destroy
-      respond_to do |format|
-        format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
-        format.json { head :no_content }
-      end
-    end
-  
-    private
+  before_action :authenticate_user!
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
-    def set_task
-        @task = current_user.tasks.find(params[:id])
-    end
-  
-    def task_params
-        params.require(:task).permit(:title, :description, :due_date, :priority)
-    end
+  def index
+    @tasks = current_user.tasks.all
+  end
 
-end  
+  def show
+  end
+
+  def new
+    @task = current_user.tasks.build
+  end
+
+  def edit
+  end
+
+  def create
+    @task = current_user.tasks.build(task_params)
+
+    if @task.save
+      respond_to_format(@task, :created)
+    else
+      respond_to do |format|
+        format.html { render :new }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    if @task.update(task_params)
+      respond_to_format(@task, :ok)
+    else
+      respond_to do |format|
+        format.html { render :edit }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @task.destroy
+    respond_to do |format|
+      format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :description, :due_date, :priority)
+  end
+
+  def respond_to_format(task, status)
+    respond_to do |format|
+      format.html { redirect_to task, notice: 'Task was successfully created.' }
+      format.json { render :show, status: status, location: task }
+    end
+  end
+end
