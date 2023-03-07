a66e17d8094090e19fedd443bed3a888b1119a2c
cleidsonmedeiros <cleidsonmedeiros12@gmail.com>
2023-03-07 12:28:11 -0300

    fix functions and add temporary css-style in html.erb

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,12 +2,19 @@ class Task < ApplicationRecord
 
     validate :due_date_cannot_be_in_the_past
     belongs_to :owner, class_name: 'User' 
-    
 
-    
+    after_save :is_expired
+
     def due_date_cannot_be_in_the_past
         if due_date.present? && due_date < Date.today
           errors.add(:due_date, "can't be in the past")
         end
+    end
+
+    def is_expired
+      if self.due_date < Date.today
+        self.status = 'expired'
+        self.save
+      end
     end
 end
